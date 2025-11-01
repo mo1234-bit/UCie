c22ce782d29a7c74279b07f232f4403781149c5b
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-11-01 22:31:17 +0200

    Delete UCie_PHY directory

--- UCie_PHY/tx_clock_forward.sv
+++
@@ -1,7 +0,0 @@
-module ucie_tx_clock_forward (
-    input wire clk, reset_n, enable,
-    output wire clkp, clkn
-);
-    assign clkp = enable ? clk : 1'b0;
-    assign clkn = enable ? ~clk : 1'b1;
-endmodule
